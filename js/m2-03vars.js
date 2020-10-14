e80d738be7821155b9fb90c7bd1a9de2437bfecd
AnnaLypska <anna.lypska@gmail.com>
2020-10-14 13:47:09 +0200

    m2-05vars

---
+++ js/m2-03vars.js
@@ -0,0 +1,19 @@
+function findLongestWord (string = "") {
+    let LongestWord ='';
+    let word = string.split(' ');
+     
+     for (let i =0; i<word.length; i+=1){
+     if (word[i].length>LongestWord.length) LongestWord = word[i]
+     }
+     return LongestWord
+     
+   }
+   
+   console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+    'jumped'
+   
+   console.log(findLongestWord('Google do a roll'));
+    'Google'
+   
+   console.log(findLongestWord('May the force be with you'));
+    'force'
